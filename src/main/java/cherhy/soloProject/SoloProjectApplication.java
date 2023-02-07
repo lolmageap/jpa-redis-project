47c2fb45895d387774453531b567955df0b0b43c
cherhy <ekxk1234@naver.com>
2023-02-07 23:23:15 +0900

    Like function and Like Cancel

--- src/main/java/cherhy/soloProject/SoloProjectApplication.java
+++ src/main/java/cherhy/soloProject/SoloProjectApplication.java
@@ -2,6 +2,7 @@ package cherhy.soloProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -12,6 +13,7 @@ import java.util.UUID;
 
 @EnableJpaAuditing
 @EnableScheduling
+@EnableCaching
 @SpringBootApplication
 public class SoloProjectApplication {
 
