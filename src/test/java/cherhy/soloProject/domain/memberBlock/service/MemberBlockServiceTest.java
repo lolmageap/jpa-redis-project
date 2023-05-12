c2c74f8f8528f39faaddd01c777ad2b946c0766f
cherhy <ekxk1234@naver.com>
2023-05-12 23:59:01 +0900

    차단하기 테스트 수정

--- src/test/java/cherhy/soloProject/domain/memberBlock/service/MemberBlockServiceTest.java
+++ src/test/java/cherhy/soloProject/domain/memberBlock/service/MemberBlockServiceTest.java
@@ -1,7 +1,11 @@
 package cherhy.soloProject.domain.memberBlock.service;
 
+import cherhy.soloProject.Util.scrollDto.ScrollRequest;
+import cherhy.soloProject.domain.member.dto.request.MemberRequest;
 import cherhy.soloProject.domain.member.entity.Member;
 import cherhy.soloProject.domain.member.service.MemberReadService;
+import cherhy.soloProject.domain.member.service.MemberWriteService;
+import cherhy.soloProject.domain.memberBlock.dto.response.MemberBlockResponseDto;
 import cherhy.soloProject.domain.memberBlock.entity.MemberBlock;
 import cherhy.soloProject.exception.MemberBlockException;
 import org.assertj.core.api.Assertions;
@@ -11,6 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,9 +29,32 @@ class MemberBlockServiceTest {
     @Autowired
     MemberReadService memberReadService;
     @Autowired
+    MemberWriteService memberWriteService;
+    @Autowired
     MemberBlockReadService memberBlockReadService;
     @Autowired
     MemberBlockWriteService memberBlockWriteService;
+
+    @BeforeEach
+    @Order(1)
+    @DisplayName("회원 추가")
+    void addMember(){
+        // given
+        MemberRequest memberRequest = new MemberRequest("abcdef", "정철희", "ekxk1234@gmail.com", "1234");
+        memberWriteService.signUp(memberRequest);
+
+        MemberRequest memberRequest2 = new MemberRequest("qwerty", "홍길동", "abcd234@naver.com", "12345");
+        memberWriteService.signUp(memberRequest2);
+
+        MemberRequest memberRequest3 = new MemberRequest("zxcvbn", "유재석", "zxcvbn@gmail.com", "1111");
+        memberWriteService.signUp(memberRequest3);
+
+        MemberRequest memberRequest4 = new MemberRequest("hihihi", "고양이", "noise@naver.com", "111111111");
+        memberWriteService.signUp(memberRequest4);
+
+        MemberRequest memberRequest5 = new MemberRequest("testtest", "유재석", "jzcxhvljk@gmail.com", "4444sdfwe");
+        memberWriteService.signUp(memberRequest5);
+    }
 
     @Test
     @Order(1)
@@ -80,6 +108,40 @@ class MemberBlockServiceTest {
             memberBlockReadService.getBlockMember(member, blockMember)
                     .orElseThrow(NullPointerException::new)
         );
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("차단한 사람들 조회")
+    void testGetMemberBlocks(){
+        // given
+        Member me = memberReadService.getMember(3L);
+        Member you = memberReadService.getMember(1L);
+        Member you2 = memberReadService.getMember(2L);
+        Member you3 = memberReadService.getMember(4L);
+        Member you4 = memberReadService.getMember(5L);
+
+        ScrollRequest scrollRequest = new ScrollRequest(null);
+
+        // when
+        MemberBlock memberToBlock1 = MemberBlock.of(me, you);
+        memberBlockWriteService.block(memberToBlock1);
+
+        MemberBlock memberToBlock2 = MemberBlock.of(me, you2);
+        memberBlockWriteService.block(memberToBlock2);
+
+        MemberBlock memberToBlock3 = MemberBlock.of(me, you3);
+        memberBlockWriteService.block(memberToBlock3);
+
+        MemberBlock memberToBlock4 = MemberBlock.of(me, you4);
+        memberBlockWriteService.block(memberToBlock4);
+
+        List<MemberBlockResponseDto> memberBlocks = memberBlockReadService.getMemberBlocks(me, scrollRequest);
+        long nextKey = memberBlockReadService.getNextKey(memberBlocks);
+
+        //then
+        Assertions.assertThat(memberBlocks.size()).isEqualTo(3);
+        Assertions.assertThat(nextKey).isEqualTo(2); // Id desc
     }
 
 }
